3c6a7b68062404676845bd8483f7e82e5f8089ae
benamreview <dhh3hb@mail.umkc.edu>
2019-02-09 15:25:46 -0600

    added initial documentation

--- ICP-3/SourceCode/In_Class_Exercise/GitHubUserFinder/script.js
+++ ICP-3/SourceCode/In_Class_Exercise/GitHubUserFinder/script.js
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param user
+ * @returns {XMLHttpRequest}
+ */
 function getGithubInfo(user) {
     //1. Create an instance of XMLHttpRequest class and send a GET request using it. The function should finally return the object(
     // it now contains the response!)
@@ -8,30 +13,41 @@ function getGithubInfo(user) {
     return xhttp;
 }
 
+/**
+ *
+ * @param user: show basic user information, number of public repositories, and his/her github url
+ */
 function showUser(user) {
 
     //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
+    //Empty the avatar and information content
     $(".avatar").empty();
     $(".information").empty();
-    console.log("in showUser");
-    console.log(user);
-    console.log(user.avatar_url);
+    //Display name
     $("#name").text("Username: " + user.login);
+    //Avatar URL
     var avatarUrl = user.avatar_url;
     var avatarImg = "<img src=\"" + avatarUrl + "\" alt=\"Trulli\" width=\"500\" height=\"333\"/>"
     console.log(avatarImg);
     $(".avatar").append(avatarImg);
+    //User information
     var informationStr = "<p style= \'font-size: 40px; font-weight: bold; \'>Profile Information</p><p> User ID: " + user.id + "</p><p>Number of Repositories: " + user.public_repos + "</p>"
     $(".information").append(informationStr);
+    //User's Github URL
     var linkStr = "<p><a id = \"link\" style = \"color:blue\" href=\"" + user.html_url + "\" target=\"_blank\" >User's Github URL</a></p>"
     console.log(linkStr);
     $(".information").append(linkStr);
 
 }
 
+/**
+ *
+ * @param username: show error message that no such user can be found
+ */
 function noSuchUser(username) {
     //3. set the elements such that a suitable message is displayed
     console.log("in noSuchUser");
+    //Reset all currently displayed information and show error message
     $(".avatar").empty();
     $(".information").empty();
     $("#name").text("ERROR: USER NOT FOUND!");
